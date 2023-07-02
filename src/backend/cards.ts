9882628ca7a02af3f540bb98bcc76ed0520c7e5b
Aslanator <30744567+aslanator@users.noreply.github.com>
2023-07-02 22:31:28 +0300

    fix: item prices

--- src/backend/cards.ts
+++ src/backend/cards.ts
@@ -1,30 +1,43 @@
-import GSheetReader from 'g-sheets-api';
-import { ItemSubType, ItemType } from '../globalTypes';
-import { ItemCard } from '../pages/Create/store';
-import { googleApiKey } from './config';
+import GSheetReader from "g-sheets-api";
+import { ItemSubType, ItemType } from "../globalTypes";
+import { ItemCard } from "../pages/Create/store";
+import { googleApiKey } from "./config";
 
 export const options = {
-    apiKey: googleApiKey,
-    sheetId: '1oFGcv4k3raSgPB33V534Reausf8hTZ5PjocrLiW1V0A',
-    sheetNumber: 1,
-    returnAllResults: false
-}
+  apiKey: googleApiKey,
+  sheetId: "1oFGcv4k3raSgPB33V534Reausf8hTZ5PjocrLiW1V0A",
+  sheetNumber: 1,
+  returnAllResults: false,
+};
 
 type RawItemCard = {
-    id: number;
-    title: string;
-    price: string;
-    image: string;
-    lowResImage: string;
-    type: ItemType;
-    subType: ItemSubType;
-    stretch?: 'HORIZONTAL' | 'VERTICAL';
-}
+  id: number;
+  title: string;
+  price: string;
+  image: string;
+  lowResImage: string;
+  type: ItemType;
+  subType: ItemSubType;
+  stretch?: "HORIZONTAL" | "VERTICAL";
+};
 
 export const getCards = async (): Promise<ItemCard[]> => {
-    return new Promise((resolve, reject) => GSheetReader(options, (result: RawItemCard[]) => {
-        resolve(result.map(item => {
-            const multiplierMatch = item.price.match(/^[x|х](\d\.\d+)$/);
-            return {...item, price: multiplierMatch ? 0 : Number(item.price), multiplier: multiplierMatch ? Number(multiplierMatch[1]) : 1};
-        }));
-    }, reject));}
+  return new Promise((resolve, reject) =>
+    GSheetReader(
+      options,
+      (result: RawItemCard[]) => {
+        resolve(
+          result.map((item) => {
+            const multiplierMatch = item.price.match(/^(x|х)(\d\.\d+)$/);
+            return {
+              ...item,
+              price: multiplierMatch ? 0 : Number(item.price),
+              multiplier: multiplierMatch ? Number(multiplierMatch[1]) : 1,
+            };
+          })
+        );
+      },
+      reject
+    )
+  );
+};
--- src/pages/Create/components/CardComponent/CardComponent.tsx
+++ src/pages/Create/components/CardComponent/CardComponent.tsx
@@ -1,20 +1,35 @@
-import { getCardStretch, ItemCard } from '../../store';
-import { HorizontalItem } from './components/HorizontalItem/HorizontalItem';
-import { VerticalItem } from './components/VerticalItem/VerticalItem';
+import { getCardStretch, ItemCard } from "../../store";
+import { HorizontalItem } from "./components/HorizontalItem/HorizontalItem";
+import { VerticalItem } from "./components/VerticalItem/VerticalItem";
 
 type Props = ItemCard & {
-    children?: React.ReactNode;
-}
+  children?: React.ReactNode;
+};
 
 export const CardComponent: React.FC<Props> = (itemCard) => {
-    const stretch = getCardStretch(itemCard);
+  const stretch = getCardStretch(itemCard);
+  console.log({ itemCard });
 
-    if(stretch === 'HORIZONTAL') {
-        return <HorizontalItem {...itemCard} price={itemCard.multiplier ? `x${itemCard.multiplier}` : itemCard.price} />
-    }
-    if(stretch === 'VERTICAL') {
-        return <VerticalItem {...itemCard} price={itemCard.multiplier ? `x${itemCard.multiplier}` : itemCard.price} />
-    }
-    
-    return null;
-}
+  if (stretch === "HORIZONTAL") {
+    return (
+      <HorizontalItem
+        {...itemCard}
+        price={
+          itemCard.multiplier > 1 ? `x${itemCard.multiplier}` : itemCard.price
+        }
+      />
+    );
+  }
+  if (stretch === "VERTICAL") {
+    return (
+      <VerticalItem
+        {...itemCard}
+        price={
+          itemCard.multiplier > 1 ? `x${itemCard.multiplier}` : itemCard.price
+        }
+      />
+    );
+  }
+
+  return null;
+};
